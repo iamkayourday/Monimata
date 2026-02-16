6ed6d12e5ae0f7b13e1a82f60a3542a6aceade83
Abdulbasit kayode imam <117276252+iamkayourday@users.noreply.github.com>
2026-02-16 11:50:32 +0100

    Update LandingPage.jsx

--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -3,7 +3,7 @@ import Navbar from '../components/Navbar'
 import Home from '../sections/Home'
 import Technology from '../sections/Technology'
 import Pricing from '../sections/Pricing'
-import Features from '../sections/Features'
+// import Features from '../sections/Features'
 import Faq from '../sections/Faq'
 import Footer from '../components/Footer'
 
