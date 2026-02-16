2b3d3e855479312e2fea5a44bddd8e875a10c4ec
Abdulbasit kayode imam <117276252+iamkayourday@users.noreply.github.com>
2026-02-16 11:48:35 +0100

    Update LandingPage.jsx

--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -14,7 +14,7 @@ const LandingPage = () => {
         <Home />
         <Technology />
         <Pricing />
-        <Features />
+      //  <Features />
         <Faq />
         <Footer />
     </div>
